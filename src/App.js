458be031b2d11dbd7b92d9509bbb299e72218ee7
anjali2670 <108923035+anjali2670@users.noreply.github.com>
2023-01-20 12:25:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Weath from './components/Weath';
+import weath from "./components/Weath"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Wearther ForeCast App</h1>
+    
+    <Weath />  </div>
+    
   );
 }
-
 export default App;
