bb6ea5fb4c8d5de07870ceb97e6ae5de5287d8fa
anjali2670 <108923035+anjali2670@users.noreply.github.com>
2023-01-20 12:47:09 +0530

    change css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Weath from './components/Weath';
-import weath from "./components/Weath"
+// import weath from "./components/Weath"
 function App() {
   return (
     <div className="App">
